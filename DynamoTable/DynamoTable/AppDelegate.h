b8566e09bc6da2fd158cc487ddbeb7c9e153074c
saumiya-uday <saumiya@SomsMacBookPro.local>
2016-09-22 23:51:37 +0530

    initial commit for dynamo table

---
+++ DynamoTable/DynamoTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DynamoTable
+//
+//  Created by Saumiya on 22/09/16.
+//  Copyright © 2016 --. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DynamoTable/DynamoTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DynamoTable
+//
+//  Created by Saumiya on 22/09/16.
+//  Copyright © 2016 --. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
